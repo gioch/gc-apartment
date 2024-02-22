44dcf2afe53ef49dbb424f8d56232e2c5a29fcfa
George Chkhvirkia <georgechkhvirkia@gmail.com>
2024-02-22 20:54:51 +0400

    Commented out application controller spec

--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-class ApplicationController < ActionController::Base
-  protect_from_forgery
+# class ApplicationController < ActionController::Base
+#   protect_from_forgery
 
-  def index; end
-end
+#   def index; end
+# end
